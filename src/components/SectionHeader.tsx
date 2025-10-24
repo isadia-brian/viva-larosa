6ca2e2e147c24b4dca0f1c6af673551abb20d845
isadia-brian <developerisadia@gmail.com>
2025-10-24 10:45:24 +0300

    update: modified .gitignore purged docs from git

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -5,7 +5,7 @@ interface SectionHeaderProps {
 
 const SectionHeader = ({ title, subtitle }: SectionHeaderProps) => {
 	return (
-		<div className="text-center max-w-3xl mx-auto flex flex-col space-y-4">
+		<div className="text-center max-w-3xl mx-auto flex flex-col space-y-4 mb-16">
 			<h2 className="font-headings text-5xl">{title}</h2>
 			<p>{subtitle}</p>
 		</div>
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -9,6 +9,19 @@ const AboutSection = () => {
 				transcends the impersonal grandeur of luxury resorts to offer something
 				far more precious: intimacy, privacy, and genuine connection."
 			/>
+			<div className="grid grid-cols-3 grid-rows-5 gap-4 md:h-[40vh]">
+				<div className="row-span-3 pr-4">
+					<p className=" text-lg font-medium leading-[20px] text-pretty">
+						Nestled in the heart of Diani, Kenya, our exclusive villa has been
+						designed for those who appreciate that true luxury lies not in
+						opulence alone, but in the art of personalized service.
+					</p>
+				</div>
+				<div className="row-span-5 bg-gray-600">3</div>
+				<div className="row-span-3 bg-blue-200">5</div>
+				<div className="col-start-1 row-start-5 bg-black">7</div>
+				<div className="row-span-2 col-start-3 row-start-4 bg-green-200">8</div>
+			</div>
 		</div>
 	);
 };
